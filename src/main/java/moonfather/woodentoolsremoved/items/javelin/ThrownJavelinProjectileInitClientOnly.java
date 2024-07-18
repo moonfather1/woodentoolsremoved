727b6b1720d9c8aa5df6de57b9d9ecef8914cbd7
MoonFather <moonfather@homemail.com>
2024-07-18 09:19:49 +0100

    javelin no longer held backwards when about to be thrown

--- src/main/java/moonfather/woodentoolsremoved/items/javelin/ThrownJavelinProjectileInitClientOnly.java
+++ src/main/java/moonfather/woodentoolsremoved/items/javelin/ThrownJavelinProjectileInitClientOnly.java
@@ -2,6 +2,8 @@ package moonfather.woodentoolsremoved.items.javelin;
 
 import moonfather.woodentoolsremoved.RegistryManager;
 import net.minecraft.client.renderer.entity.EntityRenderers;
+import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -15,6 +17,10 @@ public class ThrownJavelinProjectileInitClientOnly
 	public static void onClientSetup(FMLClientSetupEvent event)
 	{
 		EntityRenderers.register(RegistryManager.ThrownJavelinProjectileET.get(), ThrownJavelinProjectileRenderer::new);
+
+		ItemProperties.register(RegistryManager.ItemJavelin.get(), new ResourceLocation("throwing"), (itemStack, p_234997_, entity, p_234999_) -> {
+			return entity != null && entity.isUsingItem() && entity.getUseItem() == itemStack ? 1.0F : 0.0F;
+		});
 	}
 
 
