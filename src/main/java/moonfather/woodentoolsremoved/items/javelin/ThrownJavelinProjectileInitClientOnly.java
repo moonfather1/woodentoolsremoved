07ee147db6e2a56083784de475a8feb3adb232f6
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2026-05-14 19:28:44 +0800

    Wrap registration calls in enqueueWork for client setup
    Fix ArrayIndexOutOfBoundsException

--- src/main/java/moonfather/woodentoolsremoved/items/javelin/ThrownJavelinProjectileInitClientOnly.java
+++ src/main/java/moonfather/woodentoolsremoved/items/javelin/ThrownJavelinProjectileInitClientOnly.java
@@ -16,10 +16,11 @@ public class ThrownJavelinProjectileInitClientOnly
 	@SubscribeEvent
 	public static void onClientSetup(FMLClientSetupEvent event)
 	{
-		EntityRenderers.register(RegistryManager.ThrownJavelinProjectileET.get(), ThrownJavelinProjectileRenderer::new);
-
-		ItemProperties.register(RegistryManager.ItemJavelin.get(), ResourceLocation.withDefaultNamespace("throwing"), (p_234996_, p_234997_, p_234998_, p_234999_) -> {
-			return p_234998_ != null && p_234998_.isUsingItem() && p_234998_.getUseItem() == p_234996_ ? 1.0F : 0.0F;
+		event.enqueueWork(() -> {
+			EntityRenderers.register(RegistryManager.ThrownJavelinProjectileET.get(), ThrownJavelinProjectileRenderer::new);
+			ItemProperties.register(RegistryManager.ItemJavelin.get(), ResourceLocation.withDefaultNamespace("throwing"), (p_234996_, p_234997_, p_234998_, p_234999_) -> {
+				return p_234998_ != null && p_234998_.isUsingItem() && p_234998_.getUseItem() == p_234996_ ? 1.0F : 0.0F;
+			});
 		});
 	}
 
